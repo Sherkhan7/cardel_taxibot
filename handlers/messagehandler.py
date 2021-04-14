4812a62c5e2a2b8d139383ecc72dc7e751040a20
sherkhan7 <9669470@gmail.com>
2021-04-14 06:34:24 +0500

    add reply buttons     change lang     settings     back

--- handlers/messagehandler.py
+++ handlers/messagehandler.py
@@ -1,9 +1,13 @@
 from telegram import Update
 from telegram.ext import Filters, MessageHandler, CallbackContext
+
 from DB import get_user
 from helpers import wrap_tags
 from globalvariables import *
 from languages import LANGS
+
+from inlinekeyboards import InlineKeyboard
+from inlinekeyboards.inlinekeyboardvariables import *
 
 from replykeyboards import ReplyKeyboard
 from replykeyboards.replykeyboardvariables import *
@@ -14,25 +18,52 @@ import re
 def message_handler_callback(update: Update, context: CallbackContext):
     # with open('jsons/update.json', 'w') as update_file:
     #     update_file.write(update.to_json())
+    # exit()
     user = get_user(update.effective_user.id)
     user_data = context.user_data
     text = update.message.text
 
-    passenger_parcel_obj = re.search(r"(Yo'lovchi va pochta|Пассажир и почта|Йўловчи ва почта)$", text)
-    settings_obj = re.search(r"(Sozlamalar|Настройки|Созламалар)$", text)
-    contact_us_obj = re.search(r"(Biz bilan bog'lanish|Свяжитесь с нами|Биз билан боғланиш)$", text)
-    main_menu_obj = re.search(r"(Bosh menyu|Главное меню|Бош меню)$", text)
+    passenger_parcel_obj = re.search("(Yo'lovchi va pochta|Пассажир и почта|Йўловчи ва почта)$", text)
+    back_obj = re.search("(Ortga|Назад|Ортга)$", text)
+    lang_obj = re.search("(Tilni o'zgartirish|Изменить язык|Тилни ўзгартириш)$", text)
+    settings_obj = re.search("(Sozlamalar|Настройки|Созламалар)$", text)
+    contact_us_obj = re.search("(Biz bilan bog'lanish|Свяжитесь с нами|Биз билан боғланиш)$", text)
+    main_menu_obj = re.search("(Bosh menyu|Главное меню|Бош меню)$", text)
 
     if user:
 
-        if passenger_parcel_obj:
-            reply_keyboard = ReplyKeyboard(passenger_parcel_keyboard, user[LANG]).get_keyboard()
-            update.message.reply_text(text, reply_markup=reply_keyboard)
+        # if passenger_parcel_obj:
+        #     reply_keyboard = ReplyKeyboard(passenger_parcel_keyboard, user[LANG]).get_keyboard()
+        #     update.message.reply_text(text, reply_markup=reply_keyboard)
 
-        # elif settings_obj:
-        #     pass
-        #
-        # Biz bilan bo'glanish
+        # Settings button
+        if settings_obj:
+            keyboard = ReplyKeyboard(settings_keyboard, user[LANG]).get_keyboard()
+            update.message.reply_text(text, reply_markup=keyboard)
+            return
+
+        # Change lang button
+        elif lang_obj:
+            if user[LANG] == LANGS[0]:
+                text = "Tilni tanlang"
+
+            if user[LANG] == LANGS[1]:
+                text = "Выберите язык"
+
+            if user[LANG] == LANGS[2]:
+                text = "Тилни танланг"
+
+            keyboard = InlineKeyboard(langs_keyboard).get_keyboard()
+            update.message.reply_text(text, reply_markup=keyboard)
+            return
+
+        # Back button
+        elif back_obj:
+            reply_keyboard = ReplyKeyboard(main_menu_keyboard, user[LANG]).get_keyboard()
+            update.message.reply_text(text, reply_markup=reply_keyboard)
+            return
+
+        # contact us
         elif contact_us_obj:
             admin = wrap_tags('@cardel_admin')
 
