560af5a027076c5ff3b9cb45c6b0cec286217e16
sherkhan7 <9669470@gmail.com>
2021-04-18 16:43:16 +0500

    `My data` button added in Settings section and change `back` reply keyboard button's behavior when pressed

--- handlers/messagehandler.py
+++ handlers/messagehandler.py
@@ -3,10 +3,11 @@ import re
 from telegram import Update
 from telegram.ext import Filters, MessageHandler, CallbackContext
 
-from DB import get_user
+from DB import get_user, get_driver_and_car_data
 from helpers import wrap_tags
 from globalvariables import *
 from languages import LANGS
+from layouts import get_user_data_and_driver_layout
 
 from inlinekeyboards import InlineKeyboard
 from inlinekeyboards.inlinekeyboardvariables import *
@@ -20,6 +21,7 @@ def message_handler_callback(update: Update, context: CallbackContext):
     #     update_file.write(update.to_json())
     # exit()
     user = get_user(update.effective_user.id)
+    user_data = context.user_data
 
     # passenger_parcel_obj = re.search("(Yo'lovchi va pochta|Пассажир и почта|Йўловчи ва почта)$", text)
     back_obj = re.search("(Ortga|Назад|Ортга)$", update.message.text)
@@ -27,6 +29,7 @@ def message_handler_callback(update: Update, context: CallbackContext):
     settings_obj = re.search("(Sozlamalar|Настройки|Созламалар)$", update.message.text)
     contact_us_obj = re.search("(Biz bilan bog'lanish|Свяжитесь с нами|Биз билан боғланиш)$", update.message.text)
     main_menu_obj = re.search("(Bosh menyu|Главное меню|Бош меню)$", update.message.text)
+    my_data_obj = re.search("(Mening ma'lumotlarim|Мои данные|Менинг маълумотларим)$", update.message.text)
 
     if user:
 
@@ -51,9 +54,28 @@ def message_handler_callback(update: Update, context: CallbackContext):
             update.message.reply_text(text, reply_markup=keyboard)
 
             return
+        # My data button
+        elif my_data_obj:
+
+            driver = get_driver_and_car_data(user[ID])
+
+            user_data_and_driver_laybout = get_user_data_and_driver_layout(user, driver)
+            reply_keyboard = ReplyKeyboard(my_data_keyboard, user[LANG]).get_keyboard()
+            update.message.reply_html(user_data_and_driver_laybout, reply_markup=reply_keyboard)
+
+            user_data[STATE] = 'my_data'
+
+            return
         # Back button
         elif back_obj:
-            reply_keyboard = ReplyKeyboard(main_menu_keyboard, user[LANG]).get_keyboard()
+
+            keyboard = main_menu_keyboard
+
+            if STATE in user_data and user_data[STATE] == 'my_data':
+                keyboard = settings_keyboard
+                user_data.pop(STATE)
+
+            reply_keyboard = ReplyKeyboard(keyboard, user[LANG]).get_keyboard()
             update.message.reply_text(update.message.text, reply_markup=reply_keyboard)
 
             return
