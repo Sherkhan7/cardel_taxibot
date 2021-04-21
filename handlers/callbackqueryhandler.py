b58599475924add9b279d10cca79c4a85e7cd5ad
sherkhan7 <9669470@gmail.com>
2021-04-22 03:22:18 +0500

    add callback_query.answer() to alert a user

--- handlers/callbackqueryhandler.py
+++ handlers/callbackqueryhandler.py
@@ -1,10 +1,12 @@
 import logging
+import re
 
 from telegram import Update, TelegramError
 from telegram.ext import CallbackQueryHandler, CallbackContext
 
 from DB import *
 from globalvariables import *
+from languages import LANGS
 
 logger = logging.getLogger()
 
@@ -39,6 +41,24 @@ def inline_keyboards_handler_callback(update: Update, context: CallbackContext):
         callback_query.message.reply_text(reply_text)
 
         # logger.info('user_data: %s', user_data)
+        return
+
+    elif re.search(r"^dr_\d+$", data):
+
+        if user[LANG] == LANGS[0]:
+            alert_text = "Kechirasiz, faqat «Taksi qidirish» bo'limida bo'lganingizdagina lokatsiya yubora olasiz"
+        if user[LANG] == LANGS[1]:
+            alert_text = "Извините, вы можете указать местоположение, только когда находитесь в разделе «Поиск такси»"
+        if user[LANG] == LANGS[2]:
+            alert_text = "Кечирасиз, фақат «Такси қидириш» бўлимида бўлганингиздагина локация юбора оласиз"
+
+        alert_text = f'🛑 {alert_text} !'
+
+        try:
+            callback_query.answer(alert_text, show_alert=True)
+        except TelegramError:
+            pass
+
         return
 
 
